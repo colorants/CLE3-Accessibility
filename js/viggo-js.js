5bf30af38718e885ddf548a909af8ffe823740d0
colorants <viggoven@gmail.com>
2023-02-22 10:19:11 +0100

    navbar

--- js/viggo-js.js
+++
@@ -1,9 +0,0 @@
-const slider = document.querySelector('.slider');
-const range = slider.querySelector('.slider__range');
-const beforeImage = slider.querySelector('.slider__image--before');
-const overlay = slider.querySelector('.slider__overlay');
-
-range.addEventListener('input', function() {
-    beforeImage.style.width = this.value + '%';
-    overlay.style.left = this.value + '%';
-});
