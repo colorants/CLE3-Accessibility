e1b8ee00ea4fcdfef5ff523daa389bfc8ee790ff
colorants <viggoven@gmail.com>
2023-02-22 15:51:44 +0100

    changed minor css

--- js/viggo.js
+++ js/viggo.js
@@ -1,21 +1,5 @@
-const sliderContainer = document.querySelector('.slider-container');
-const beforeImage = document.querySelector('.before-image');
-const afterImage = document.querySelector('.after-image');
-const sliderHandle = document.querySelector('.slider-handle');
-
-function moveHandle(e) {
-    const x = e.pageX - sliderContainer.offsetLeft;
-    if (x > 0 && x < sliderContainer.offsetWidth) {
-        sliderHandle.style.left = x + 'px';
-        beforeImage.style.width = x + 'px';
-        afterImage.style.width = sliderContainer.offsetWidth - x + 'px';
-    }
+var divisor = document.getElementById("divisor"),
+    slider = document.getElementById("slider");
+function moveDivisor() {
+    divisor.style.width = slider.value+"%";
 }
-
-sliderHandle.addEventListener('mousedown', function() {
-    document.addEventListener('mousemove', moveHandle);
-});
-
-document.addEventListener('mouseup', function() {
-    document.removeEventListener('mousemove', moveHandle);
-});
