3dbf2820ad4d36e7ba8e836544eaea736f627355
colorants <viggoven@gmail.com>
2023-03-08 14:25:52 +0100

    changed js

--- js/viggo.js
+++ js/viggo.js
@@ -1,5 +1,3 @@
-document.addEventListener("load", init);
-
 let divisor = document.getElementById("divisor"),
     slider = document.getElementById("slider");
 
@@ -7,10 +5,6 @@ function moveDivisor() {
     divisor.style.width = slider.value+"%";
 }
 
-function init() {
-    document.divisor.style.backgroundImage("url('/images/viggo/no_blur_1.jpg')");
-    document.divisor.style.backgroundImage("url('/images/viggo/no_blur_1.jpg')");
-    slider.oninput(moveDivisor);
-}
+
 
 
