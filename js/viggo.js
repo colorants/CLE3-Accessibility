45049e822b4a5652a846b95d159a07c7250b9f33
colorants <viggoven@gmail.com>
2023-03-08 10:48:31 +0100

    changed js

--- js/viggo.js
+++ js/viggo.js
@@ -1,5 +1,16 @@
-var divisor = document.getElementById("divisor"),
+document.addEventListener("load", init);
+
+let divisor = document.getElementById("divisor"),
     slider = document.getElementById("slider");
+
 function moveDivisor() {
     divisor.style.width = slider.value+"%";
 }
+
+function init() {
+    document.divisor.style.backgroundImage("url('/images/viggo/no_blur_1.jpg')");
+    document.divisor.style.backgroundImage("url('/images/viggo/no_blur_1.jpg')");
+    slider.oninput(moveDivisor);
+}
+
+
