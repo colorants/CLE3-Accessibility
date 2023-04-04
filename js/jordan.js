9034b451c916183eb5818d7c2bf4a04ed08dc800
Jordan <jr.goedbloed@gmail.com>
2023-04-04 22:41:23 +0200

    Added 3 images to use as eventlistener in jordan.html Added function in javascript for eventlistener

--- js/jordan.js
+++ js/jordan.js
@@ -1,7 +1,22 @@
-let JSONItems = [];
-let $;
+// let myObj = {
+//     name: "Zara",
+//     location: "Zuid-Holland",
+//     number: 638999990
+//
+// };
+//
+// let myObj_serialized = JSON.stringify(myObj)
+//
+// localStorage.setItem("myObj", myObj_serialized);
+//
+// let myObj_deserialized = JSON.parse(localStorage.getItem("myObj"));
+//
+// console.log(myObj_deserialized)
 
-$.toJSON("js/jordan.json", function (data) {
-    JSONItems = data;
-    console.log(JSONItems)
-})
+const buttonTwo = document.querySelector('.btn-2');
+
+function alertBtn () {
+    alert('Klik OK om door te gaan naar de navigatie');
+}
+
+buttonTwo.addEventListener("click", alertBtn);
