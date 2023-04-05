b24298cb30dad1032cd5de380c5d5445dfb5134b
colorants <viggoven@gmail.com>
2023-04-05 10:36:53 +0200

    finished html and css, js functions.

--- js/viggo.js
+++ js/viggo.js
@@ -1,23 +1,70 @@
-const slider = document.getElementById("slider");
-const divisor = document.getElementById("divisor");
-const image1 = document.getElementById("img-1");
-const image2 = document.getElementById("img-2");
-const image3 = document.getElementById("img-3");
-const button1 = document.getElementById('button1');
-const button2 = document.getElementById('button2');
-const button3 = document.getElementById('button3');
-const opacity = "1";
-const noOpacity = "0";
-const resetButton = document.getElementById("resetButton");
+let apiUrl = "http://localhost/CLE3-Accessibility/webservice/viggo-webservice.php"
+
+window.addEventListener("load", init); // wait for the page to load
+
+// variables
+let slider = document.getElementById("slider");
+let divisor = document.getElementById("divisor");
+
+let image1;
+let image2;
+let image3;
+let button1;
+let button2;
+let button3;
+const opacity = 1;
+const noOpacity = 0;
+let resetButton;
+
+// event listeners
+function init() {
 
 
+    image1 = document.getElementById("img-1");
+    image2 = document.getElementById("img-2");
+    image3 = document.getElementById("img-3");
+    button1 = document.getElementById('dot-1');
+    button2 = document.getElementById('dot-2');
+    button3 = document.getElementById('dot-3');
+    resetButton = document.getElementById("resetButton");
 
-window.addEventListener('load', init);
-
-// this function will be called when the page is loaded
-function init () {
-    imageSlider();
     resetButton.addEventListener('click', reset);
+    moveDivisor();
+    imageSlider();
+}
+
+
+// function to change the opacity of the images
+    // add event listeners to the buttons
+function imageSlider(){
+    // add event listeners to the buttons
+    button1.addEventListener('click', () => {
+        image1.style.opacity = opacity;
+        image2.style.opacity = noOpacity;
+        image3.style.opacity = noOpacity;
+        button1.classList.add("active");
+        button2.classList.remove("active");
+        button3.classList.remove("active");
+    });
+
+    button2.addEventListener('click', () => {
+        image1.style.opacity = noOpacity;
+        image2.style.opacity = opacity;
+        image3.style.opacity = noOpacity;
+        button1.classList.remove("active");
+        button2.classList.add("active");
+        button3.classList.remove("active");
+    });
+
+    button3.addEventListener('click', () => {
+        console.log("button 3 clicked");
+        image1.style.opacity = noOpacity;
+        image2.style.opacity = noOpacity;
+        image3.style.opacity = opacity;
+        button1.classList.remove("active");
+        button2.classList.remove("active");
+        button3.classList.add("active");
+    });
 }
 
 // reset button
@@ -28,61 +75,26 @@ function reset() {
 }
 
 // move the divisor when the slider is moved
-
 function moveDivisor() {
     localStorage.setItem('sliderValue', slider.value);
     divisor.style.width = slider.value + "%";
-    console.log("Slider value after setting local storage:", slider.value);
 }
+
+// Set the slider value to the stored value when the slider is moved
+slider.addEventListener("input", function() {
+    moveDivisor();
+    let storedValue = localStorage.getItem('sliderValue');
+    slider.value = storedValue;
+});
 
 window.addEventListener("load", function() {
     const storedValue = localStorage.getItem('sliderValue');
     if (storedValue) {
         divisor.style.width = storedValue + "%";
         slider.value = storedValue;
-        console.log("Slider value after setting width and value:", slider.value);
     }
 });
 
-// Set the slider value to the stored value when the slider is moved
-slider.addEventListener("input", function() {
-    moveDivisor();
-   let storedValue = localStorage.getItem('sliderValue');
-    slider.value = storedValue;
-});
-
-// 3 dotted image slider , dots change on click
-function imageSlider() {
-
-// add event listeners to the buttons
-button1.addEventListener('click', () => {
-   image1.style.opacity = opacity;
-   image2.style.opacity = noOpacity;
-   image3.style.opacity = noOpacity;
-   button1.classList.add("active");
-   button2.classList.remove("active");
-    button3.classList.remove("active");
-
-});
-
-button2.addEventListener('click', () => {
-    image1.style.opacity = noOpacity;
-    image2.style.opacity = opacity;
-    image3.style.opacity = noOpacity;
-    button1.classList.remove("active");
-    button2.classList.add("active");
-    button3.classList.remove("active");
-});
-
-button3.addEventListener('click', () => {
-    image1.style.opacity = noOpacity;
-    image2.style.opacity = noOpacity;
-    image3.style.opacity = opacity;
-     button1.classList.remove("active");
-    button2.classList.remove("active");
-    button3.classList.add("active");
-});
-}
 
 
 
---
+++ webservice/includes/viggo-actions.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @return array
+ */
+function getInfo()
+{
+    return [
+        [
+            "id" => 1,
+            "name" => "Hoe ziet een slechtziend persoon in een winkelstraat",
+            "description" => "Wat valt op? (10% zicht)",
+        ],
+        [
+            "id" => 2,
+            "name" => "Hoe ziet een persoon met Glaucoom en verkeers situatie",
+            "description" => "Bij glaucoom wordt de oogzenuw beschadigd. Hierdoor ontstaat geleidelijk verlies van het gezichtsveld",
+        ],
+    ];
+}
+
+/**
+ * @param $id
+ * @return mixed
+ */
+function getInfoDetails($id)
+{
+    $tags = [
+        1 => [
+            "name" => "Glaucoom",
+            "detailedDescription" => "Meestal is de oogboldruk bij glaucoom te hoog, maar de oogboldruk kan ook normaal zijn. Dat heet ‘normale druk glaucoom’. De behandeling bestaat uit het verlagen van de oogdruk. Ook bij normale druk glaucoom is dat vaak nodig."
+        ],
+    ];
+
+    return $tags[$id];
+}
